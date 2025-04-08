4eeec35207964297b589178846a898019a2dbadc
GurraDesu <gustaf.styf.4726@student.uu.se>
2025-04-08 13:22:33 +0200

    created MyTexture header (RAII) and Constants header

---
+++ Constants.hpp
@@ -0,0 +1,5 @@
+#pragma warning(push)
+#pragma warning(disable:ALL_CODE_ANALYSIS_WARNINGS)
+#include "raylib.h"
+#pragma warning(pop)
+
---
+++ MyTexture.hpp
@@ -0,0 +1,8 @@
+#pragma warning(push)
+#pragma warning(disable:ALL_CODE_ANALYSIS_WARNINGS)
+#include "raylib.h"
+#pragma warning(pop)
+
+class MyTextur final {
+	
+};
