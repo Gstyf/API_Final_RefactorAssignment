4e69efe3b7a7b41fc5798eba426ea6a3588e8f04
GurraDesu <gustaf.styf.4726@student.uu.se>
2025-04-08 14:00:36 +0200

    Created throwable Texture Contructor

--- MyTexture.hpp
+++ MyTexture.hpp
@@ -2,7 +2,27 @@
 #pragma warning(disable:ALL_CODE_ANALYSIS_WARNINGS)
 #include "raylib.h"
 #pragma warning(pop)
+#include <string>
+#include <format>
+#include <stdexcept>
 
-class MyTextur final {
-	
+class MyTexture final {
+	Texture2D myTex;
+
+	explicit MyTexture(std::string_view path)
+	{
+		myTex = LoadTexture(path.data());
+		if (myTex.id <= 0)
+		{
+			throw std::runtime_error(std::format("Failed to load image at {}", path));
+		}
+	}
+	MyTexture(const MyTexture&) = delete;
+	MyTexture(MyTexture&&) = delete;
+	MyTexture& operator=(const MyTexture&) = delete;
+	MyTexture& operator=(MyTexture&&) = delete;
+	~MyTexture()
+	{
+		UnloadTexture(myTex);
+	}
 };
